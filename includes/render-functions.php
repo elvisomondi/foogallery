2ab4436bd9fe51ca6c6f15ef0c94191f72025a35
Brad Vincent <bradvin@gmail.com>
2026-02-12 15:57:19 +0000

    always include custom rel if it's set

--- includes/render-functions.php
+++ includes/render-functions.php
@@ -135,8 +135,6 @@ function foogallery_build_attachment_html_anchor_attributes( $foogallery_attachm
 		$url = $foogallery_attachment->url;
 	}
 
-	$has_custom_url = 'custom' === $link && ! empty( $url );
-
 	// fallback for images that might not have a custom url.
 	if ( empty( $url ) ) {
 		$url = $foogallery_attachment->url;
@@ -150,7 +148,7 @@ function foogallery_build_attachment_html_anchor_attributes( $foogallery_attachm
 		if ( ! empty( $foogallery_attachment->custom_target ) && 'default' !== $foogallery_attachment->custom_target ) {
 			$attr['target'] = $foogallery_attachment->custom_target;
 		}
-		if ( $has_custom_url && ! empty( $foogallery_attachment->custom_rel ) ) {
+		if ( ! empty( $foogallery_attachment->custom_rel ) ) {
 			$attr['rel'] = $foogallery_attachment->custom_rel;
 		}
 	}
